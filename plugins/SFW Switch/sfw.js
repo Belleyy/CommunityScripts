524b48b051d5f05ce1da541c229a6970cbed6428
Belleyy <Belleyy@users.noreply.github.com>
2022-12-11 18:08:12 +0100

    Update sfw.js

--- plugins/SFW Switch/sfw.js
+++ plugins/SFW Switch/sfw.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /*
 //https://stackoverflow.com/a/56825511
 const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
@@ -22,13 +20,13 @@ const BUTTON_CSS = `
 `;
 */
 
-function main() {
+function create_sfwswitch_button() {
     if (!document.getElementById("plugin_sfw")) {
         var plugin_div = document.createElement('a');
         plugin_div.className = "mr-2";
         plugin_div.innerHTML = '<button id="plugin_sfw" type="button" class="minimal d-flex align-items-center h-100 btn btn-primary" title="Turn SFW Mode"><svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="svg-inline--fa fa-cog fa-w-16 fa-icon undefined" viewBox="1.5 1.5 13 13">  <path d="m7.646 9.354-3.792 3.792a.5.5 0 0 0 .353.854h7.586a.5.5 0 0 0 .354-.854L8.354 9.354a.5.5 0 0 0-.708 0z"></path><path d="M11.414 11H14.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5h-13a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .5.5h3.086l-1 1H1.5A1.5 1.5 0 0 1 0 10.5v-7A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v7a1.5 1.5 0 0 1-1.5 1.5h-2.086l-1-1z"></path></svg></button>';
-        waitForElementClass("logout-button", function () { //#CSS
-            var main_Div = document.getElementsByClassName("logout-button")[1].parentElement; //#CSS
+        waitForElementClass("navbar-buttons", function () { //#CSS
+            var main_Div = document.getElementsByClassName("navbar-buttons")[0]; //#CSS
             main_Div.insertBefore(plugin_div, main_Div.childNodes[0]);
             document.getElementById("plugin_sfw").addEventListener("click", sfw_mode, false);
             sfw_mode();
@@ -36,7 +34,7 @@ function main() {
     }
 }
 
-function sfw_mode() {
+function sfw_switch() {
     var stash_css = find_stashcss();
     if (stash_css.disabled) {
         console.log("Enable SFW");
@@ -73,4 +71,4 @@ function waitForElementClass(elementId, callBack, time) {
 }
 
 //addCSS(BUTTON_CSS);
-main();
+create_sfwswitch_button();
